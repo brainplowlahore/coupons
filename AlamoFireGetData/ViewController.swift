7ce1b4b6ea4163dae8c66b599a6329fb29989738
Faizan <faizan.nasir147@gmail.com>
2019-10-28 12:12:26 +0500

    Initial Commit

---
+++ AlamoFireGetData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamoFireGetData
+//
+//  Created by Admin on 28/10/2019.
+//  Copyright © 2019 Brain Plow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
